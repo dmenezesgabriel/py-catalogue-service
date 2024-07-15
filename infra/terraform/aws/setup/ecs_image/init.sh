3fbe5b853725d7f2a49342e7164ec341bce8816e
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-07-15 15:17:44 -0300

    chore: change ecs image terraform backend key

--- infra/terraform/aws/setup/ecs_image/init.sh
+++ infra/terraform/aws/setup/ecs_image/init.sh
@@ -4,7 +4,7 @@ ACCOUNT_ID=$(aws sts get-caller-identity --output json | grep -o '"Account": "[^
 echo "Caller identity accofunt: $ACCOUNT_ID"
 terraform init \
     -backend-config="bucket=tf-state-py-order-system-catalogue-${ACCOUNT_ID}" \
-    -backend-config="key=streamlit_ecs_image_state/terraform.tfstate" \
+    -backend-config="key=ecs_image_state/terraform.tfstate" \
     -backend-config="region=us-east-1" \
     -backend-config="dynamodb_table=tf-state-py-order-system-catalogue-${ACCOUNT_ID}" \
     -backend-config="encrypt=false" \
